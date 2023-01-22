27271ba67f0778efcbdfe998ff8263021ef06a31
closeai <closeai2000@gmail.com>
2023-01-22 19:56:44 +0300

    clear

---
+++ task19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+/*
+Console.WriteLine("Введите пятизначное число:");
+string num = Convert.ToString(Console.ReadLine());
+int length = num.Length;
+if (length == 5) 
+{
+ string num5 = num.Substring(4, 1);
+ string num4 = num.Substring(3, 1);
+ string num2 = num.Substring(1, 1);
+ string num1 = num.Substring(0, 1);
+  if (num5 == num1 && num4 == num2) Console.Write("Число - палиндром ");
+  else Console.Write("Число не палиндром");
+}
+else Console.Write ("Число не пятизначное");
+*/
+Console.WriteLine ("Введите пятизначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int num5 = num % 10;
+int num4 = num / 10 % 10;
+int num2 = num / 1000 % 10;
+int num1 = num / 10000;
+if (num < 100000 && num >= 10000)
+{
+  if (num5 == num1 && num4 == num2) Console.Write("Число - палиндром");
+  else Console.Write("Число не палиндром");
+} 
+else Console.Write ("Число не пятизначное ");
---
+++ task21/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.WriteLine("input x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y2: ");
+int y2  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double Length = Math.Sqrt((Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2)));
+Console.WriteLine("Расстояние между точками равно:" + Length);
