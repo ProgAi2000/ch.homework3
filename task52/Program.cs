f8338483c1333c72f5c8d4670f9c2a313c95ff20
closeai <closeai2000@gmail.com>
2023-02-10 22:25:33 +0300

    recursion

--- task52/Program.cs
+++ task52/Program.cs
@@ -7,33 +7,33 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-Console.Write("Введите количество строк массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество строк массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] arr = new int[m, n];
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        arr[i,j] = new Random().Next(1, 10);
-    }
-}
-
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j],4}");
+            arr[i,j] = new Random().Next(1, 10);
         }
-        Console.WriteLine();
     }
-}
 
-PrintArray(arr);
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    PrintArray(arr);
 
 Console.Write("Среднее арифметическое каждого столбца: ");
 for (int j = 0; j < arr.GetLength(1); j++)
---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите чило N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(SumOfNumbers(N,M));
+
+int SumOfNumbers(int start, int end)
+{   int sum = 0;
+    if (start == end + 1 ) return sum;
+
+    return (sum = start + SumOfNumbers(start+1,end));
+}
